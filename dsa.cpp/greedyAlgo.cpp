cfbb1e9a6f94338070bb89eac8e92086c318c614
Garima2307 <ggupta230705@gmail.com>
2026-05-11 17:49:18 +0530

    indian coins

--- dsa.cpp/greedyAlgo.cpp
+++ dsa.cpp/greedyAlgo.cpp
@@ -101,23 +101,98 @@
 
 
 //mini absolute differenece
+// #include <iostream>
+// #include <vector> 
+// #include <algorithm>
+// #include <cstdlib>
+// using namespace std;
+
+// int main(){
+//     vector <int> A = {1,2,3};
+//     vector <int> B = {2,1,3};
+
+//     sort(A.begin() , A.end());
+//     sort(B.begin() , B.end());
+
+//     int ans =0;
+//     for(int i=0 ; i<A.size() ; i++){
+//         ans += abs(A[i] - B[i]);
+//     }
+//     cout << ans << endl;
+//     return 0;
+// }
+
+
+
+
+
+
+//max length chaiin of pairs
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
+// using namespace std;
+
+// bool compare(pair <int,int>p1 , pair<int , int> p2){
+//     return p1.second < p2.second;
+// }
+
+// int maxChainLen(vector<pair<int,int>> pairs){
+//     int n = pairs.size();
+//     sort(pairs.begin() , pairs.end() , compare);
+//     int ans = 1;
+//     int currEnd = pairs[0].second;
+
+//     for(int i=1 ; i<n ; i++){
+//         if(pairs[i].first > currEnd){
+//             ans++;
+//             currEnd = pairs[i].second;
+//         }
+//     }
+//     cout << ans << endl;
+//     return ans;
+// }
+
+// int main(){
+//     int n = 5;
+//     vector<pair<int,int>> pairs(n , make_pair(0,0));
+//     pairs[0] = make_pair(5,24);
+//     pairs[1] = make_pair(39,60);
+//     pairs[2] = make_pair(5,28);
+//     pairs[3] = make_pair(27,40);
+//     pairs[4] = make_pair(50,90);
+//     maxChainLen(pairs);
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+//indian coins
 #include <iostream>
-#include <vector> 
-#include <algorithm>
-#include <cstdlib>
+#include <vector>
 using namespace std;
 
-int main(){
-    vector <int> A = {1,2,3};
-    vector <int> B = {2,1,3};
+int getMinChange(vector<int> coins , int V){
+    int ans=0;
+    int n = coins.size();
 
-    sort(A.begin() , A.end());
-    sort(B.begin() , B.end());
-
-    int ans =0;
-    for(int i=0 ; i<A.size() ; i++){
-        ans += abs(A[i] - B[i]);
+    for(int i=n-1 ; i>=0 && V>0 ; i--){
+        if(V >= coins[i]){
+            ans += V / coins[i];
+            V = V % coins[i];
+        }
     }
-    cout << ans << endl;
+    cout << ans;
+    return 0;
+}
+int main(){
+    vector <int> coins = {1,2,5,10,20,50,100,500,2000};
+    int V = 590;
+    getMinChange(coins , V);
     return 0;
 }
