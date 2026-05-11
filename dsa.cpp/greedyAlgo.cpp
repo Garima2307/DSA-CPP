a3ded0ca9c988b898aa9a772f276b55ca2e3c931
Garima2307 <ggupta230705@gmail.com>
2026-05-11 16:59:04 +0530

    absolute diff

--- dsa.cpp/greedyAlgo.cpp
+++ dsa.cpp/greedyAlgo.cpp
@@ -51,46 +51,73 @@
 
 
 //FRACTIONAL KNAPSACK QUE
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+
+// bool compare(pair<double , int> p1 , pair<double,int> p2){
+//     return p1.first > p2.first ;
+// }
+// int fractionalKnapsack(vector<int> val , vector<int> weight , int W){
+//     int n=val.size();
+//     vector<pair<double,int>> ratio(n , make_pair(0.0 , 0));
+
+//     for(int i=0 ; i<n ; i++){
+//         double r = val[i] / (double)weight[i];
+//         ratio[i] = make_pair(r , i);
+//     }
+//     sort(ratio.begin() , ratio.end() , compare);
+
+//     int ans =0;
+//     for(int i=0 ; i<n ; i++){
+//         int idx = ratio[i].second;
+//         if(weight[idx] <= W){
+//             ans += val[idx];
+//             W -= weight[idx];
+//         }else{
+//             ans += ratio[i].first*W;
+//             W =0;
+//             break;
+//         }
+
+//     }
+//     cout << ans << endl;
+//     return ans;
+// }
+// int main(){
+//     vector<int> val = {60,100,120};
+//     vector<int> weight = {10,20,30};
+
+//     int W = 50;
+//     fractionalKnapsack(val , weight , W);
+//     return 0;
+// }
+
+
+
+
+
+
+
+//mini absolute differenece
 #include <iostream>
-#include <vector>
+#include <vector> 
 #include <algorithm>
+#include <cstdlib>
 using namespace std;
 
-bool compare(pair<double , int> p1 , pair<double,int> p2){
-    return p1.first > p2.first ;
-}
-int fractionalKnapsack(vector<int> val , vector<int> weight , int W){
-    int n=val.size();
-    vector<pair<double,int>> ratio(n , make_pair(0.0 , 0));
+int main(){
+    vector <int> A = {1,2,3};
+    vector <int> B = {2,1,3};
 
-    for(int i=0 ; i<n ; i++){
-        double r = val[i] / weight[i];
-        ratio[i] = make_pair(r , i);
-    }
-    sort(ratio.begin() , ratio.end() , compare);
+    sort(A.begin() , A.end());
+    sort(B.begin() , B.end());
 
     int ans =0;
-    for(int i=0 ; i<n ; i++){
-        int idx = ratio[i].second;
-        if(weight[idx] <= W){
-            ans += val[idx];
-            W -= weight[idx];
-        }else{
-            ans += ratio[i].first*W;
-            W =0;
-            break;
-        }
-
+    for(int i=0 ; i<A.size() ; i++){
+        ans += abs(A[i] - B[i]);
     }
     cout << ans << endl;
-    return ans;
-}
-int main(){
-    vector<int> val = {60,100,120};
-    vector<int> weight = {10,20,30};
-
-    int W = 50;
-    fractionalKnapsack(val , weight , W);
     return 0;
 }
-
