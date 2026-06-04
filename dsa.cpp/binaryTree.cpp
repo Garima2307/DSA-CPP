b0e693041c5b5ff78829ad6f3ec51fca7f1d7903
Garima2307 <ggupta230705@gmail.com>
2026-06-04 11:30:15 +0530

    added lowest common ancestor

--- dsa.cpp/binaryTree.cpp
+++ dsa.cpp/binaryTree.cpp
@@ -234,10 +234,48 @@ void kthLevel(Node* root , int k){
     cout << endl;
     
 }
+
+//lowest common ancestor
+bool rootToNode(Node* root , int n , vector<int> &path){    //pass by reference so that changes will be made in org path vectors
+    if(root == NULL){
+        return false;
+    }
+
+    path.push_back(root->data);
+    if(root->data == n){
+        return true;
+    }
+    int isLeft = rootToNode(root->left , n , path);
+    int isRight = rootToNode(root->right , n , path);
+    
+    if(isLeft || isRight){
+        return true;
+    }
+    path.pop_back();
+    return false;
+}
+int LCA(Node* root , int n1 , int n2){
+    vector<int> path1;
+    vector<int> path2;
+
+    rootToNode(root , n1 , path1);
+    rootToNode(root , n2 , path2);
+
+    int lca = -1;
+    for(int i=0,j=0 ; i<path1.size() ,j<path2.size() ; i++,j++){
+        if(path1[i] != path2[j]){
+            return lca;
+        }
+        lca = path1[i];
+    }
+}
+
 int main(){
     vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     Node* root = build(nodes);
-    kthLevel(root , 2);
+    
+    int n1 = 4 , n2 = 5;
+    cout << LCA(root , n1 , n2) << endl;
     
     return 0;
 }
