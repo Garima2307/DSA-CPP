81174fe1b44f005db23e7eb94c991c2240412b5a
Garima2307 <ggupta230705@gmail.com>
2026-06-06 16:17:43 +0530

    merge 2 BST que added

--- dsa.cpp/binarySearchTree.cpp
+++ dsa.cpp/binarySearchTree.cpp
@@ -270,6 +270,34 @@ Node* balancedBST(Node* root){
 
     return BSTfromSortedVec(nodes , 0 , nodes.size()-1);
 }
+
+//MERGE 2 BST
+Node* mergeBST(Node* root1 , Node* root2){
+    vector<int> nodes1;
+    vector<int> nodes2;
+    vector<int> merged;
+
+    getInorder(root1 , nodes1);
+    getInorder(root2 , nodes2);
+
+    int i=0 , j=0;
+    while(i<nodes1.size() && j<nodes2.size()){
+        if(nodes1[i] < nodes2[j]){
+            merged.push_back(nodes1[i++]);
+        }else{
+            merged.push_back(nodes2[j++]);
+        }
+    }
+    while(i<nodes1.size()){
+        merged.push_back(nodes1[i++]);
+    }
+    while(j<nodes2.size()){
+        merged.push_back(nodes2[j++]);
+    }
+   
+    return BSTfromSortedVec(merged , 0 , merged.size() - 1);
+
+}
 int main(){
     // int arr[7] = {3,4,5,6,7,8,9};
     // Node* root = buildBSTfromSorted(arr,0,6);
@@ -277,16 +305,29 @@ int main(){
     // cout << endl;
 
 
-    Node* root = new Node(6);
-    root->left = new Node(5);
-    root->left->left = new Node(4);
-    root->left->left->left = new Node(3);
+    // Node* root = new Node(6);
+    // root->left = new Node(5);
+    // root->left->left = new Node(4);
+    // root->left->left->left = new Node(3);
     
-    root->right = new Node(7);
-    root->right->right= new Node(8);
-    root->right->right->right = new Node(9);
+    // root->right = new Node(7);
+    // root->right->right= new Node(8);
+    // root->right->right->right = new Node(9);
 
-    root = balancedBST(root);
+    // root = balancedBST(root);
+    // preorder(root);
+    // cout << endl;
+
+    
+    Node* root1 = new Node(2);
+    root1->left = new Node(1);
+    root1->right = new Node(4);
+
+    Node* root2 = new Node(9);
+    root2->left = new Node(3);
+    root2->right = new Node(12);
+
+    Node* root = mergeBST(root1 , root2);
     preorder(root);
     cout << endl;
     return 0;
